dd0c926b788adc7054226d981a15b00b95222b4e
Steven Walter <stevenrwalter@gmail.com>
2024-02-24 12:35:58 -0500

    Use a hidden parameter for edit/delete

--- procedures.sql
+++ procedures.sql
@@ -9,7 +9,7 @@ BEGIN
 END //
 
 DROP PROCEDURE IF EXISTS modifyLog //
-CREATE PROCEDURE modifyLog (logNumber INT, pilot VARCHAR(255), day DATE, endTach DECIMAL(8,2), endHobbs DECIMAL(8,2), startOil DECIMAL(4,2), oilAdded DECIMAL(4,2), note VARCHAR(255), OUT result VARCHAR(255))
+CREATE PROCEDURE modifyLog (_entry INT, pilot VARCHAR(255), day DATE, endTach DECIMAL(8,2), endHobbs DECIMAL(8,2), startOil DECIMAL(4,2), oilAdded DECIMAL(4,2), note VARCHAR(255), OUT result VARCHAR(255))
 BEGIN
     UPDATE logs as l SET
         l.pilot = pilot,
@@ -19,14 +19,14 @@ BEGIN
         l.startOil = startOil,
         l.oiladded = oilAdded,
         l.note = note
-        WHERE l.entry = logNumber;
+        WHERE l.entry = _entry;
     SET result = "Success";
 END //
 
 DROP PROCEDURE IF EXISTS deleteLog //
-CREATE PROCEDURE deleteLog (logEntry INT, OUT result VARCHAR(255))
+CREATE PROCEDURE deleteLog (_entry INT, OUT result VARCHAR(255))
 BEGIN
-    DELETE FROM logs WHERE logs.entry = logEntry;
+    DELETE FROM logs WHERE logs.entry = _entry;
     SET result = "Success";
 END //
 
